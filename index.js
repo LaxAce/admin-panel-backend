a732fb7df6957017a2df0680b9ad0696e322847f
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2022-04-22 00:51:26 +0100

    make it better

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ dotenv.config();
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-})
 
 // routes
 app.use('/api/v1/signup', signup);

4f8f18b4271ad3f8c1d5a13906d0b206f72dfeea
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2022-04-24 03:01:09 +0100

    make it better

--- controller/users.js
+++ controller/users.js
@@ -1,42 +1,41 @@
-import User from '../models/user.js';
+import User from "../models/user.js";
 
 const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.status(200).json({nbHits: users.length, users});
-    } catch (error) {
-        res.status(500).json({msg: error});
-    };
+  try {
+    const users = await User.find({});
+    res.status(200).json({ nbHits: users.length, users });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateUser = async (req, res) => {
-    try {
-        const {id: userId} = req.params;
-        const updatedUser = await User.findOneAndUpdate(
-            {_id: userId}, 
-            req.body, 
-            {
-            now: true,
-            runValidator: true
-        });
-        if (!updatedUser) {
-            return res.status(404).json({msg: `No user with the id: ${id}`});
-        }
-        res.status(200).json({msg: 'User updated successfully', updatedUser});
-    } catch (error) {
-        res.status(500).json({status: '500', msg: error});
+  try {
+    const { id: userId } = req.params;
+    const updatedUser = await User.findOneAndUpdate({ _id: userId }, req.body, {
+      now: true,
+      runValidator: true,
+    });
+    if (!updatedUser) {
+      return res.status(404).json({ msg: `No user with the id: ${id}` });
     }
+    res.status(200).json({ msg: "User updated successfully", updatedUser });
+  } catch (error) {
+    res.status(500).json({ status: "500", msg: error });
+  }
 };
 
 const updateManyUsers = async (req, res) => {
-    try {
-        const updatedUsers = await User.updateMany({}, req.body, {now: true})
-        if (!updatedUsers) res.status(404).json({msg: `No user found`});
-        res.status(200).json({msg: 'User updated successfully', updatedUsers})
-    } catch (error) {
-        
-    }
-}
+  try {
+    const updatedUsers = await User.updateMany({ selected: true }, req.body, {
+      now: true,
+    });
+    if (!updatedUsers) res.status(404).json({ msg: `No user found` });
+    res.status(200).json({ msg: "User updated successfully", updatedUsers });
+  } catch (error) {
+    res.status(500).json({ status: "500", msg: error });
+  }
+};
 
 // const deleteUser = async (req, res) => {
 //     try {
@@ -51,18 +50,13 @@ const updateManyUsers = async (req, res) => {
 // }
 
 const deleteUsers = async (req, res) => {
-    try {
-        const deletedUsers = await User.deleteMany({selected: true})
-        if (!deletedUsers) res.status(404).json({msg: 'No user found'});
-        res.status(200).json({msg: 'Users deleted successfully', user: null})
-    } catch (error) {
-        res.status(500).json({msg: error});
-    }
-}
+  try {
+    const deletedUsers = await User.deleteMany({ selected: true });
+    if (!deletedUsers) res.status(404).json({ msg: "No user found" });
+    res.status(200).json({ msg: "Users deleted successfully", user: null });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
-export {
-    getAllUsers,
-    updateUser,
-    updateManyUsers,
-    deleteUsers,
-}
+export { getAllUsers, updateUser, updateManyUsers, deleteUsers };

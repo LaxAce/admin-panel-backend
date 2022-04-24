cca271c7c1cd0515c147a623fa3997d0458e2a92
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2022-04-24 03:09:21 +0100

    make it better

--- controller/login.js
+++ controller/login.js
@@ -1,26 +1,23 @@
-import User from '../models/user.js';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 dotenv.config();
 
 const login = async (req, res) => {
-    try {
-        const {email, password} = req.body;
-        const user = await User.login(email, password);
-        
-        // create token
-        const token = jwt.sign(
-            {userId: user._id, email},
-            process.env.TOKEN_KEY,
-            {expiresIn: '2h'}
-        );
+  try {
+    const { email, password } = req.body;
+    const user = await User.login(email, password);
 
-        user.token = token;
-        res.status(200).json({user})
-    } catch (error) {
-        res.status(500).json({msg: error})
-    }
+    // create token
+    const token = jwt.sign({ userId: user._id, email }, process.env.TOKEN_KEY, {
+      expiresIn: "30d",
+    });
 
+    user.token = token;
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 export default login;

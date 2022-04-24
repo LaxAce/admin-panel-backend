47a475a9391a6daf2b7c77e23624877abf146725
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2022-04-24 03:10:04 +0100

    make it better

--- controller/signup.js
+++ controller/signup.js
@@ -1,35 +1,35 @@
-import User from '../models/user.js';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 dotenv.config();
 
 const signup = async (req, res) => {
-    try {
-        const {name, email, password} = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-        if(!(name && email && password)) res.status(400).json({msg: 'All input is required'});
+    if (!(name && email && password))
+      res.status(400).json({ msg: "All input is required" });
 
-        const oldUser = await User.findOne({email});
-        if (oldUser) res.status(409).json({msg: 'User already exist. Please login'});
+    const oldUser = await User.findOne({ email });
+    if (oldUser)
+      res.status(409).json({ msg: "User already exist. Please login" });
 
-        const user = await User.create({
-            name,
-            email: email.toLowerCase(),
-            password,
-        });
+    const user = await User.create({
+      name,
+      email: email.toLowerCase(),
+      password,
+    });
 
-        // create token
-        const token = jwt.sign(
-            {userId: user._id, email},
-            process.env.TOKEN_KEY,
-            {expiresIn: '2h'}
-        );
-        
-        user.token = token;
-        res.status(200).json({user});
-    } catch (error) {
-        res.status(500).json({msg: error})
-    }
+    // create token
+    const token = jwt.sign({ userId: user._id, email }, process.env.TOKEN_KEY, {
+      expiresIn: "30d",
+    });
+
+    user.token = token;
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 export default signup;
